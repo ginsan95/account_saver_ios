cc710b1edb8f326353503768651faa1200bd3fe2
Avery Choke <avery.choke@orion.co.com>
2018-01-04 22:57:03 +0800

    Initial Commit

---
+++ AccountSaver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccountSaver
+//
+//  Created by Avery Choke on 4/1/18.
+//  Copyright © 2018 P4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
